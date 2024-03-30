830b1973f44b16c38b455728da070edf78872c69
Brian Jahnke <bjahnke71@gmail.com>
2024-03-29 21:03:41 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    let sum = 0;
+    if (typeof numA !== 'number' || typeof numB !== 'number') {
+        return 'ERROR';
+    }
+    if (numA < 0 || numB < 0) {
+        return 'ERROR';
+    }
+    let start = Math.min(numA, numB);
+    let end = Math.max(numA, numB);
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
