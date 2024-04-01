be2a8e6329a59eb206a3985557ea4e6f9247f7f8
Brian Jahnke <bjahnke71@gmail.com>
2024-04-01 15:12:02 -0400

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+function round(value, decimals) {
+  return Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals);
+}
+
+const convertToCelsius = function(f) {
+  return round(((f - 32) * 5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return round(c * 9 / 5 + 32, 1);
 };
 
 // Do not edit below this line
