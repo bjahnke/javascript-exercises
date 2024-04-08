8fb009773dcc8ffb0c7c300e9ac75876edf82053
Brian Jahnke <bjahnke71@gmail.com>
2024-04-08 15:43:53 -0400

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num < 0) {
+        return "OOPS"
+    }
+    if (num < 2) {
+        return num;
+    }
+    let prevFib2 = 0;
+    let prevFib1 = 1;
+    let fib = prevFib1;
+    for (let i = 2; i <= num; i++) {
+        fib = prevFib1 + prevFib2;
+        prevFib2 = prevFib1;
+        prevFib1 = fib;
+    }
+    return fib;
 
 };
 
