aa05295a7c5e90ac44e68cd22c174cff217eb9cf
Brian Jahnke <bjahnke71@gmail.com>
2024-04-08 18:53:26 -0400

    Complete exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+function getAge(person){
+    let yearOfDeath = !person.yearOfDeath ? (new Date()).getFullYear() : person.yearOfDeath;
+    return yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(poeple) {
+    return poeple.reduce(
+        (oldest, item) => getAge(oldest) > getAge(item) ? oldest : item
+    );
 };
 
 // Do not edit below this line
