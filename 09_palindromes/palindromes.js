193613d6cd0e096a0754dd389727f56a779a9894
Brian Jahnke <bjahnke71@gmail.com>
2024-04-08 15:28:56 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strFormatted = str.toLowerCase().match(/[a-z0-9]/g).join('');
 
+    const reversed = strFormatted.split('').reverse().join('');
+    return strFormatted === reversed;
 };
 
 // Do not edit below this line
